f34c0c4b9bbe84cddb4debda03b37366ca5c53c6
essam11 <esam.m7md@yahoo.com>
2016-12-30 00:58:12 +0200

    save one 30-12

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,64 @@
+class ArticlesController < ApplicationController
+   before_action :set_article,only: [:edit ,:update ,:destroy,:show]
+
+   def home
+
+   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+
+  end
+
+  def destroy
+
+    @article.destroy
+    redirect_to articles_path
+    flash[:danger] = "ِYour Tip has been Deleted"
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @article.update(article_params)
+       flash[:success] = "ِYour Tip has been Updated"
+       redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
+  def show
+
+
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+       flash[:success]="Your Tip has been Created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_article
+   @article = Article.find(params[:id])
+  end
+
+  def article_params
+  params.require(:article).permit(:title,:description)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlphaBlog::Application.config.secret_key_base = 'bbc96df7df905574c294929ce420ccb8a1382f10983f41c298fe69bc8c129c29530730ad5d98a7a1fcec08c1af2c6dd4941a5f3d31c1470a8d9fbed71ac4cb5d'
