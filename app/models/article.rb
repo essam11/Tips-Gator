c79a936511f5293a8349ca2e76a26e2ad9839076
essam11 <esam.m7md@yahoo.com>
2016-12-30 19:22:24 +0200

    increase the number of letter in title of tips

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-   validates :title ,presence: true ,length: {minimum: 5 ,maximum: 50}
+   validates :title ,presence: true ,length: {minimum: 5 ,maximum: 100}
    validates :description ,presence: true ,length: {minimum: 5 ,maximum: 500}
 
 end
