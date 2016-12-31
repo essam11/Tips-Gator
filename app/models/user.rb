c5954db95abaaed363fdd9a6df35ec336fa9b70b
essam11 <esam.m7md@yahoo.com>
2016-12-31 16:05:47 +0200

    Create and validate users

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+  validates :username ,presence: true ,uniqueness: true,length: {minimum: 3,maximum: 25}
+  validates :email ,presence: true,uniqueness: true,
+end
+  
