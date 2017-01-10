5c7a469f8c37eef9550d033b7556f6c30d0335a1
essam11 <esam.m7md@yahoo.com>
2017-01-09 22:27:50 -0800

    login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ def new
   def create
   @user=User.new(user_params)
   if @user.save
+    session[:user_id]=@user.id
     flash[:success]="Welcome to Tips Gator #{@user.username}"
-    redirect_to articles_path
+    redirect_to user_path(@user)
   else
     render "new"
   end
